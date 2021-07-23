b15fd72cf7a8277147d0957c663012abf71509d4
cudnart99 <tkchoiboi456@gmail.com>
2021-07-24 01:30:08 +0700

    push

---
+++ router/src/components/checkProp.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import React from "react";
+
+
+export default class Greeting extends React.Component {
+  render() {
+    return (
+      <h1>Hello, {this.props.name}</h1>
+    );
+  }
+}
+
+Greeting.propTypes = {
+  name: PropTypes.bool
+};
